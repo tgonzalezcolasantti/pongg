c84459829a2e95f1adb9aa7614573f97d45cdf26
marcos727 <marco.enrique.garrido@gmail.com>
2026-07-16 17:20:13 -0300

    character.h first

---
+++ src/include/character.h
@@ -0,0 +1,24 @@
+#ifndef __CHARACTER_H
+#define __CHARACTER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string>
+
+typedef struct character_t
+{
+    string name;
+    string job;
+    int srtrenght;
+    int dexterity;
+    int constitucion;
+    int intelligence;
+    int wisdom;
+    int charisma;
+} character_t;
+
+
+
+
+
+#endif
