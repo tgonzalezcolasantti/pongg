11035f976612575fe587b123d4c4ea7c7673756d
marcos727 <marco.enrique.garrido@gmail.com>
2026-07-17 00:53:39 -0300

    falta menooosss

--- src/include/character.h
+++ src/include/character.h
@@ -10,11 +10,17 @@ typedef struct character_t
     string name;
     string job;
     int strength;
+    int strength_mod;
     int dexterity;
+    int dexterity_mod;
     int constitucion;
+    int constitucion_mod;
     int intelligence;
+    int intelligence_mod;
     int wisdom;
+    int wisdom_mod;
     int charisma;
+    int charisma_mod;
 } character_t;
 
 
